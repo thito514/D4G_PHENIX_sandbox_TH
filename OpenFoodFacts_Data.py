4c01b7977ce746d673d88b05841e8bf0833eaa60
unknown <thierry.ha@gmail.com>
2018-10-24 23:22:24 +0200

    update

--- OpenFoodFacts_Data.py
+++ OpenFoodFacts_Data.py
@@ -5,7 +5,7 @@ def get_OpenFoodFactsData(EAN_number):
 	import requests
 
 	url = ' https://fr.openfoodfacts.org/api/v0/produit/'
-	product_json = requests.get(url+EAN_search+'.json').content
+	product_json = requests.get(url+str(EAN_number)+'.json').content
 	df_product = pd.read_json(product_json)
 
 	return df_product
